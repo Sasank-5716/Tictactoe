533f8dd89da0884e68ea60a9e0bd803803a30319
Sasank5716 <sasanklama5716@gmail.com>
2025-04-12 23:22:13 +0545

    Add color constants and game state variables

--- tictactoe.py
+++ tictactoe.py
@@ -7,3 +7,13 @@ pygame.init()
 WIDTH, HEIGHT = 600, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic-Tac-Toe")
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+LINE_COLOR = (50, 50, 50)
+
+# Game board
+board = [['', '', ''], ['', '', ''], ['', '', '']]
+current_player = 'X'
+game_over = False
