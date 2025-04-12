9f1290d8e90ddd6262a3387b933ca9b02d294c97
Sasank5716 <sasanklama5716@gmail.com>
2025-04-12 23:20:52 +0545

    first commit

---
+++ tictactoe.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+
+pygame.init()
+
+# Screen dimensions
+WIDTH, HEIGHT = 600, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tic-Tac-Toe")
